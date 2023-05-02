7d739457df86ef210eb0fd021594ea4530d42c67
Abdullah Mohamed Elmasry <114958723+AbdullahElmasry@users.noreply.github.com>
2023-05-02 23:55:13 +0300

    DIO_AVR_DRIVER
    FROM ZERO TO HERO

---
+++ DIO_AVR_DRIVER/Debug/COTS/APP/main.d
@@ -0,0 +1,12 @@
+COTS/APP/main.o COTS/APP/main.o: ../COTS/APP/main.c \
+  ../COTS/APP/../LIB/BIT_MATH.h ../COTS/APP/../LIB/STD_TYPES.h \
+  ../COTS/APP/../MCAL/1-DIO/DIO_interface.h \
+  ../COTS/APP/../MCAL/1-DIO/DIO_config.h
+
+../COTS/APP/../LIB/BIT_MATH.h:
+
+../COTS/APP/../LIB/STD_TYPES.h:
+
+../COTS/APP/../MCAL/1-DIO/DIO_interface.h:
+
+../COTS/APP/../MCAL/1-DIO/DIO_config.h:
---
+++ DIO_AVR_DRIVER/Debug/COTS/APP/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../COTS/APP/main.c \
+../COTS/APP/tempCodeRunnerFile.c 
+
+OBJS += \
+./COTS/APP/main.o \
+./COTS/APP/tempCodeRunnerFile.o 
+
+C_DEPS += \
+./COTS/APP/main.d \
+./COTS/APP/tempCodeRunnerFile.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+COTS/APP/%.o: ../COTS/APP/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: AVR Compiler'
+	avr-gcc -Wall -g2 -gstabs -O0 -fpack-struct -fshort-enums -ffunction-sections -fdata-sections -std=gnu99 -funsigned-char -funsigned-bitfields -mmcu=atmega32 -DF_CPU=16000000UL -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
