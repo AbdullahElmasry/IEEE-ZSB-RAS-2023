9ef26a6226fcbb6d462a29a316bd2dcc7e7f7f56
Abdullah Mohamed Elmasry <114958723+AbdullahElmasry@users.noreply.github.com>
2023-07-26 01:54:08 +0300

    main.c
    1st Update to main.c

--- Electronic_Switches/APP/main.c
+++ Electronic_Switches/APP/main.c
@@ -16,9 +16,20 @@
 #define pass "5000"
 #define pass_L 4
 
+//TAKE OPTION FROM USER
+void Get_User_Option(u8 *number)
+{
+	while(505){
+		KPD_u8GetKeyState(number);
+		if(*number == '1' || *number == '2' || *number == '3'){
+			break;
+		}
+	}
+}
+
 u8 main(void)
 {
-	// CLEAR SCREEEN
+	//CLEAR SCREEEN
 	LCD_voidSendCmnd(0x01);
 	_delay_ms(2);
 	LCD_u8GoToXY(LCD_u8_LINE1,0);
@@ -28,8 +39,9 @@ u8 main(void)
 	u8 index = 0;
 	u8 Local_u8key;
 	u8 pressed_keys[pass_L];
+	u8 number;
 
-	// initialization of DIO
+//	// initialization of DIO
 	DIO_voidInit();
 	// initialization of LCD
 	LCD_voidInit();
@@ -85,41 +97,78 @@ u8 main(void)
 				 _delay_ms(2);
 				LCD_u8GoToXY(LCD_u8_LINE1,0);
 				LCD_SendString("CORRECT PASSWORD");
-				_delay_ms(500);
-				while(505){
+				_delay_ms(1000);
 				// CLEAR SCREEEN
 				LCD_voidSendCmnd(0x01);
 				 _delay_ms(2);
 				LCD_u8GoToXY(LCD_u8_LINE1,0);
-				LCD_SendString("WELCOME TO M");
+				LCD_SendString("WELCOME TO MOTOR");
 				LCD_u8GoToXY(LCD_u8_LINE2,2);
 				LCD_SendString("DASHBOARD");
+				_delay_ms(1000);
 				// CLEAR SCREEEN
 				LCD_voidSendCmnd(0x01);
 				 _delay_ms(2);
 				LCD_u8GoToXY(LCD_u8_LINE1,0);
-				LCD_SendString("1-DC MOTOR");
+				LCD_SendString("1-DC MOTOR 3-EXT");
 				LCD_u8GoToXY(LCD_u8_LINE2,0);
 				LCD_SendString("2-STR MOTOR");
 
 
+				Get_User_Option(&number);
+				if(number == '1') // DC Motor
+				{
+					// CLEAR SCREEEN
+					LCD_voidSendCmnd(0x01);
+					_delay_ms(2);
+					LCD_SendString("1-CLOCK WISE");
+					LCD_u8GoToXY(LCD_u8_LINE2,0);
+					LCD_SendString("2-C CLOCK WISE");
+					Get_User_Option(&number);
+					if(number == '1'){DCM_u8_ROTATE_CW();}
+					else{DCM_u8_ROTATE_CCW();}
+					// CLEAR SCREEEN
+					LCD_voidSendCmnd(0x01);
+					_delay_ms(2);
+					LCD_SendString("END TASK");
+					break;
+				}
+				else if(number == '2') // Stepper Motor
+				{
+					// CLEAR SCREEEN
+					LCD_voidSendCmnd(0x01);
+					_delay_ms(2);
+					LCD_SendString("1-CLOCK WISE");
+					LCD_u8GoToXY(LCD_u8_LINE2,0);
+					LCD_SendString("2-C CLOCK WISE");
+					Get_User_Option(&number);
+					if(number == 1){STM_u8_Rotate_CW();}
+					else{STM_u8_Rotate_CCW();}
+					// CLEAR SCREEEN
+					LCD_voidSendCmnd(0x01);
+					_delay_ms(2);
+					LCD_SendString("END TASK");
+					break;
 
+				}
+				else{
+					// CLEAR SCREEEN
+					LCD_voidSendCmnd(0x01);
+					_delay_ms(2);
+					LCD_SendString("END TASK");
+					break;
 				}
 			}
 		}
-
-
 	}
 
 
 
+
+
+//this loop for testing
 //	while(1){
-//	STM_u8_Rotate_CW();
-//
-//	_delay_ms(500);
-//	STM_u8_Rotate_CCW();
-//
-//	_delay_ms(500);
+//	DCM_u8_ROTATE_CW();
 //	}
 	return 0;
 }
